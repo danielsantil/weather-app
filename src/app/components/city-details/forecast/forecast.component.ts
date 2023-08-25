373da6f81549d7bed2ad4e54f5dd07e85f5079df
Daniel Santil <danielsantilh@gmail.com>
2023-08-25 02:16:06 -0400

    adding gradient lines to forecast chart

--- src/app/components/city-details/forecast/forecast.component.ts
+++ src/app/components/city-details/forecast/forecast.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { WeatherDetailsBaseComponent } from "../weather-details-base.component";
 import { WeatherService } from "src/app/services/open-weather/weather.service";
 import { Weather, WeatherUnit } from "src/app/models/weather";
 import { ForecastResponse } from "src/app/models/forecast";
-import { Chart } from "chart.js/auto";
+import { Chart, ScriptableLineSegmentContext, TooltipItem } from "chart.js/auto";
 import { SettingsService } from "src/app/services/settings/settings.service";
 import { DatePipe } from "@angular/common";
 
@@ -17,6 +17,10 @@ export class ForecastComponent implements WeatherDetailsBaseComponent, OnInit {
   chart: Chart;
   tempUnit: WeatherUnit;
   forecastCount: number;
+  blue = 'rgb(54, 162, 235)';
+  red = 'rgb(255, 0, 0)';
+  yellow = 'rgb(255, 150, 0)';
+  gray = 'rgb(110, 110, 110)';
 
   constructor(
     private weatherService: WeatherService,
@@ -30,49 +34,34 @@ export class ForecastComponent implements WeatherDetailsBaseComponent, OnInit {
   }
 
   getData(): void {
-    if (this.chart)
-      this.chart.destroy();
-
     this.weatherService.getForecast(this.model.id, this.forecastCount).subscribe({
       next: response => {
         this.forecast = response;
         const data = this.forecast.list.map(x => {
-          let date = new Date(x.dt * 1000);
-          let label = this.datePipe.transform(date, 'yyyy-MM-dd ha');
-          let val = x.main.temp;
-          return { x: label, y: val };
+          const date = new Date(x.dt * 1000);
+          const label = this.datePipe.transform(date, 'MMM-dd ha');
+          return { x: label, y: x.main.temp };
         });
-        const colors = this.forecast.list.map(x => {
-          if (this.isCool(x.main.temp))
-            return 'rgb(54, 162, 235)';
-          if (this.isHot(x.main.temp))
-            return 'rgb(255, 0, 0)';
-          else
-            return 'rgb(255, 150, 0)';
-        });
-        this.createChart(data, colors);
+        this.createChart(data);
       }
     });
   }
+  
+  createChart(data: any[]): void {
+    if (this.chart)
+      this.chart.destroy();
 
-  createChart(data: any[], colors: string[]): void {
     this.chart = new Chart('forecast-chart', {
       type: 'line',
       data: {
         datasets: [{
           label: 'Forecast',
           data: data,
-          backgroundColor: colors,
+          tension: 0.1,
+          pointRadius: 5,
+          borderColor: this.gray,
           segment: {
-            borderColor: (context) => {
-              const xVal = context.p0.parsed.y;
-              if (this.isCool(xVal))
-              return 'rgb(54, 162, 235)';
-            if (this.isHot(xVal))
-              return 'rgb(255, 0, 0)';
-            else
-              return 'rgb(255, 150, 0)';
-            }
+            borderColor: (ctx) => this.createGradientLine(ctx)
           }
         }]
       },
@@ -81,7 +70,7 @@ export class ForecastComponent implements WeatherDetailsBaseComponent, OnInit {
           tooltip: {
             callbacks: {
               title: () => 'Forecast Info',
-              label: (context) => this.onTooltipLabelHover(context)
+              label: (ctx) => this.onTooltipLabelHover(ctx)
             }
           },
           legend: {
@@ -92,8 +81,8 @@ export class ForecastComponent implements WeatherDetailsBaseComponent, OnInit {
     });
   }
 
-  onTooltipLabelHover(context: any): string | string[] {
-    const model = this.forecast.list[context.dataIndex];
+  onTooltipLabelHover(ctx: TooltipItem<any>): string | string[] {
+    const model = this.forecast.list[ctx.dataIndex];
     return [
       `Date: ${new Date(model.dt * 1000).toDateString()}`,
       `Time: ${new Date(model.dt * 1000).toLocaleTimeString()}`,
@@ -101,15 +90,35 @@ export class ForecastComponent implements WeatherDetailsBaseComponent, OnInit {
     ];
   }
 
+  createGradientLine(ctx: ScriptableLineSegmentContext): CanvasGradient {
+    const temp1 = ctx.p0.parsed.y;
+    const temp2 = ctx.p1.parsed.y;
+
+    const gradient = this.chart.canvas.getContext('2d')!
+        .createLinearGradient(ctx.p0.x, ctx.p0.y, ctx.p1.x, ctx.p1.y);
+    gradient.addColorStop(0, this.getTempColor(temp1));
+    gradient.addColorStop(1, this.getTempColor(temp2));
+
+    return gradient;
+  }
+
+  getTempColor(temp: number): string {
+    if (this.isCool(temp))
+      return this.blue;
+    if (this.isHot(temp))
+      return this.red;
+    return this.yellow;
+  }
+
   isCool(temp: number): boolean {
     return this.tempUnit == WeatherUnit.C && temp < 18
-      || this.tempUnit == WeatherUnit.F && temp < 63
-      || this.tempUnit == WeatherUnit.K && temp < 290;
+      || this.tempUnit == WeatherUnit.F && temp < 64.4
+      || this.tempUnit == WeatherUnit.K && temp < 291.15;
   }
 
   isHot(temp: number): boolean {
     return this.tempUnit == WeatherUnit.C && temp > 30
       || this.tempUnit == WeatherUnit.F && temp > 86
-      || this.tempUnit == WeatherUnit.K && temp > 303;
+      || this.tempUnit == WeatherUnit.K && temp > 303.15;
   }
 }
